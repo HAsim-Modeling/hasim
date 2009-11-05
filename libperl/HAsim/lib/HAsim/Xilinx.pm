2e18d2a15e2c3971454ee8d3da6de16c541b542c
Michael Adler <michael.adler@intel.com>
2009-11-05 01:28:24 +0000

    CSN: CSN-hasim-718
    Generate a build tree in which each region can be synthesized by Xst
    independently, treating all others as black boxes.
    
    hasim-gen-black-box takes a Bluespec generated Verilog wrapper and turns
    it into a black box Verilog wrapper.  The script is really simple.  If
    Bluespec Verilog generation changes it will have to be updated.

--- libperl/HAsim/lib/HAsim/Xilinx.pm
+++ libperl/HAsim/lib/HAsim/Xilinx.pm
@@ -76,7 +76,8 @@ sub generate_files_xilinx {
 
     # PRJ file
 
-    my $final_prj_file = HAsim::Util::path_append($builddir,$xilinx_config_dir,$name . ".prj");
+    my $config_path = HAsim::Util::path_append($builddir, $xilinx_config_dir);
+    my $final_prj_file = HAsim::Util::path_append($config_path, $name . ".prj_base");
     open(PRJFILE, "> $final_prj_file") || return undef;
 
     my $xilinx_processfile = sub {
@@ -106,38 +107,36 @@ sub generate_files_xilinx {
         }
     };
 
+    ##
+    ## Start by generating a single, master, prj file that loads only stub
+    ## files.  The master file will then be replicated for each synthesis
+    ## boundary, replacing the stub for a synthesis boundary with the true
+    ## wrapper.
+    ##
+    generate_prj_file($model, $builddir, *PRJFILE{IO}, $xilinx_processfile);
+    my @model_prj_files = find_all_files_with_suffix($model->modelroot(), ".prj");
+    generate_concatenation_file($model, $name, $builddir, *PRJFILE{IO}, @model_prj_files);
+    close(PRJFILE);
+
+    ## Replicate template to each true prj file
+    replicate_prj_files($model->modelroot(), $name, $config_path);
+    # Base file was just a template that is no longer needed
+    unlink($final_prj_file);
 
 
-    ## XST file
-
-    my $final_xst_file = HAsim::Util::path_append($builddir,$xilinx_config_dir,$name . ".xst");
-
-    open(XSTFILE, "> $final_xst_file") || return undef;
-
+    ## XST files.  One xst file per prj file.
     my @model_xst_files = find_all_files_with_suffix($model->modelroot(), ".xst");
+    gen_xst_synth_files($model, $builddir, $model->modelroot(), $config_path, @model_xst_files);
+
 
     ## UT File
-
     my $final_ut_file = HAsim::Util::path_append($builddir,$xilinx_config_dir,$name . ".ut");
-
     open(UTFILE, "> $final_ut_file") || return undef;
 
     #Concatenate all found .ut files
-    
     my @model_ut_files = find_all_files_with_suffix($model->modelroot(), ".ut");
-
-    ##PRJ Files    
-
-    my @model_prj_files = find_all_files_with_suffix($model->modelroot(), ".prj");
-
-
-
-    generate_prj_file($model, $builddir, *PRJFILE{IO}, $xilinx_processfile);
-    generate_concatenation_file($model, $builddir, *PRJFILE{IO}, @model_prj_files);
-    generate_concatenation_file($model, $builddir, *XSTFILE{IO}, @model_xst_files);
-    generate_concatenation_file($model, $builddir, *UTFILE{IO}, @model_ut_files);
+    generate_concatenation_file($model, $name, $builddir, *UTFILE{IO}, @model_ut_files);
     generate_download_file($model, $builddir);
-
 }
 
 
@@ -216,10 +215,10 @@ sub generate_files_synplify {
     print SDFFILE "set_option -constraint -enable \"\$env(BUILD_DIR)/config/$name.sdc\"\n";
 
     # gather any user sdf files
-    generate_concatenation_file($model, $builddir, *SDFFILE{IO}, @model_sdf_files);   
+    generate_concatenation_file($model, $name, $builddir, *SDFFILE{IO}, @model_sdf_files);   
  
     #build global sdf file
-    generate_concatenation_file($model, $builddir, *SDCFILE{IO}, @model_sdc_files);   
+    generate_concatenation_file($model, $name, $builddir, *SDCFILE{IO}, @model_sdc_files);   
 
 
 }
@@ -231,11 +230,10 @@ sub generate_files_synplify {
 
 sub generate_concatenation_file {
     my $model = shift;
+    my $name = shift;
     my $builddir = shift;
     my $file = shift;
     my @files = shift;    
-
-    my $name = HAsim::Build::get_model_name($model);
 
     my $replacements_r = HAsim::Util::empty_hash_ref();
 
@@ -303,12 +301,13 @@ sub __generate_prj_file {
         my $n_instances = HAsim::Build::synthesis_instances($module);
         if ($n_instances == 0) {
             # Single instance
-            $processfile->($module,$file,$my_dir . "/" . $HAsim::Bluespec::tmp_bsc_dir, HAsim::Build::get_wrapper($module) . ".v");
+            my $wrapper = HAsim::Build::get_wrapper($module);
+            $processfile->($module,$file,$my_dir . "/" . $HAsim::Bluespec::tmp_bsc_dir, "${wrapper}_stub.v");
         }
         else {
             for (my $i = 0; $i < $n_instances; $i++) {
                 my $wrapper = "mk_" . HAsim::Build::make_instance_wrapper_name($module->provides(), $i);
-                $processfile->($module,$file,$my_dir . "/" . $HAsim::Bluespec::tmp_bsc_dir, "${wrapper}.v");
+                $processfile->($module,$file,$my_dir . "/" . $HAsim::Bluespec::tmp_bsc_dir, "${wrapper}_stub.v");
             }
         }
     }
@@ -338,7 +337,101 @@ sub __generate_prj_file {
             $processfile->($module,$file,"",$v_file);
 	}
     }
-	    
+}
+
+
+############################################################
+# replicate_prj_files:
+#   Copy master prj file for each synthesis boundary, tweaking the file
+#   to build the right code.
+sub replicate_prj_files {
+    my $module = shift;
+    my $base_name = shift;
+    my $config_path = shift;
+
+    # recurse
+    HAsim::Build::check_submodules_defined($module);
+    foreach my $child ($module->submodules()) {
+        replicate_prj_files($child, $base_name, $config_path);
+    }
+
+    if (HAsim::Build::is_synthesis_boundary($module)) {
+        my $n_instances = HAsim::Build::synthesis_instances($module);
+        if ($n_instances == 0) {
+            # Single instance
+            my $wrapper = HAsim::Build::get_wrapper($module);
+            prj_file_for_synth_boundary("${config_path}/${base_name}.prj_base",
+                                        "${config_path}/${wrapper}.prj",
+                                        $wrapper);
+        }
+        else {
+            for (my $i = 0; $i < $n_instances; $i++) {
+                my $wrapper = "mk_" . HAsim::Build::make_instance_wrapper_name($module->provides(), $i);
+                prj_file_for_synth_boundary("${config_path}/${base_name}.prj_base",
+                                            "${config_path}/${wrapper}.prj",
+                                            $wrapper);
+            }
+        }
+    }
+}
+
+sub prj_file_for_synth_boundary {
+    my $src_file = shift;
+    my $dst_file = shift;
+    my $wrapper = shift;
+
+    open(SRC, "< $src_file") || return undef;
+    open(DST, "> $dst_file") || return undef;
+
+    while (<SRC>) {
+        chomp;
+
+        # Replace stub with true wrapper Verilog file for synth boundary
+        my $p = $_;
+        my $match = $HAsim::Bluespec::tmp_bsc_dir . "/" . $wrapper;
+        $p =~ s/${match}_stub.v/${match}.v/;
+
+        print DST "$p\n";
+    }
+
+    close(SRC);
+}
+
+
+############################################################
+# gen_xst_synth_files:
+#   Generate an .xst file for each synthesis boundary.
+sub gen_xst_synth_files {
+    my $model = shift;
+    my $builddir = shift;
+    my $module = shift;
+    my $config_path = shift;
+    my @model_xst_files = shift;
+
+    # recurse
+    HAsim::Build::check_submodules_defined($module);
+    foreach my $child ($module->submodules()) {
+        gen_xst_synth_files($model, $builddir, $child, $config_path, @model_xst_files);
+    }
+
+    if (HAsim::Build::is_synthesis_boundary($module)) {
+        my $n_instances = HAsim::Build::synthesis_instances($module);
+        if ($n_instances == 0) {
+            # Single instance
+            my $wrapper = HAsim::Build::get_wrapper($module);
+            open(XSTFILE, "> ${config_path}/${wrapper}.xst") || return undef;
+            generate_concatenation_file($model, $wrapper, $builddir, *XSTFILE{IO}, @model_xst_files);
+            close(XSTFILE);
+        }
+        else {
+            for (my $i = 0; $i < $n_instances; $i++) {
+                my $wrapper = "mk_" . HAsim::Build::make_instance_wrapper_name($module->provides(), $i);
+                open(XSTFILE, "> ${config_path}/${wrapper}.xst") || return undef;
+                generate_concatenation_file($model, $wrapper, $builddir, *XSTFILE{IO}, @model_xst_files);
+                close(XSTFILE);
+            }
+        }
+    }
 }
 
 
